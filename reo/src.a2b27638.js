480573dec035afd613da18449034926a5993b52f
manac <mcoromandel@hotmail.com>
2024-03-24 14:51:47 +1300

    Add files via upload

--- reo/src.a2b27638.js
+++ reo/src.a2b27638.js
@@ -44893,6 +44893,10 @@ var WordDropComponent = exports.WordDropComponent = /*#__PURE__*/function (_Even
     _classPrivateFieldSet(_assertThisInitialized(_this), _allWords, []);
     _Categories.default.forEach(function (cat) {
       cat.words.forEach(function (word) {
+        //create a temp or defaut url for the word to be used when 
+        if (word.url == '') {
+          word.url = '';
+        }
         _classPrivateFieldGet(_assertThisInitialized(_this), _allWords).push(word);
       });
     });
@@ -44929,7 +44933,7 @@ var WordDropComponent = exports.WordDropComponent = /*#__PURE__*/function (_Even
     key: "categories",
     set: function set(categories) {
       var _this2 = this;
-      console.clear();
+      //  console.clear();
       console.log('missing word start check');
       categories.forEach(function (cat) {
         cat.words.forEach(function (word) {
@@ -44977,6 +44981,9 @@ var WordDropComponent = exports.WordDropComponent = /*#__PURE__*/function (_Even
       //store the word in the custom list
       var category = _classPrivateFieldGet(this, _categoriesHash)[_classPrivateFieldGet(this, _currentWord).url];
       _classPrivateFieldGet(this, _currentWord).selected = true;
+
+      //add saved word to incorrect answers
+      _classPrivateFieldGet(this, _incorrectAnswers)[_classPrivateFieldGet(this, _currentWord).maori] = _classPrivateFieldGet(this, _currentWord);
       _Controller.C.addToMyList(category, _classPrivateFieldGet(this, _currentWord));
       return {
         category: category,
@@ -45360,7 +45367,7 @@ function _checkAnswer2() {
       //check to see if there any errors
       var errorCount = Object.keys(_classPrivateFieldGet(this, _incorrectAnswers)).length;
       console.log('errors', Object.keys(_classPrivateFieldGet(this, _incorrectAnswers)).length, _classPrivateFieldGet(this, _activeWords), _classPrivateFieldGet(this, _quizWordCount), errorCount, _classPrivateFieldGet(this, _incorrectAnswers));
-      var message = errorCount ? [_classPrivateFieldGet(this, _quizWordCount) - errorCount + '/' + _classPrivateFieldGet(this, _quizWordCount) + ' correct', 'Practice Again?'] : ['Tika', 'Practice Again?'];
+      var message = errorCount ? [_classPrivateFieldGet(this, _quizWordCount) - errorCount + '/' + _classPrivateFieldGet(this, _quizWordCount) + ' correct.', 'Practice mistakes again?'] : ['Tika', 'Practice again?'];
       //reset errors
       _classPrivateFieldSet(this, _incorrectAnswers, {});
       _classPrivateFieldSet(this, _words, _classPrivateMethodGet(this, _shuffle, _shuffle2).call(this, _classPrivateFieldGet(this, _words)));
@@ -45371,7 +45378,7 @@ function _checkAnswer2() {
         stle: {}
       }, {
         label: 'NO'
-      }], this, function (context) {
+      }], this, function (context, response) {
         context.loadRandomWord();
       });
     } else {
@@ -46633,17 +46640,17 @@ function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
-function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
-function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : String(i); }
+function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
+function _callSuper(t, o, e) { return o = _getPrototypeOf(o), _possibleConstructorReturn(t, _isNativeReflectConstruct() ? Reflect.construct(o, e || [], _getPrototypeOf(t).constructor) : o.apply(t, e)); }
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } else if (call !== void 0) { throw new TypeError("Derived constructors may only return object or undefined"); } return _assertThisInitialized(self); }
+function _isNativeReflectConstruct() { try { var t = !Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); } catch (t) {} return (_isNativeReflectConstruct = function _isNativeReflectConstruct() { return !!t; })(); }
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 function _get() { if (typeof Reflect !== "undefined" && Reflect.get) { _get = Reflect.get.bind(); } else { _get = function _get(target, property, receiver) { var base = _superPropBase(target, property); if (!base) return; var desc = Object.getOwnPropertyDescriptor(base, property); if (desc.get) { return desc.get.call(arguments.length < 3 ? target : receiver); } return desc.value; }; } return _get.apply(this, arguments); }
 function _superPropBase(object, property) { while (!Object.prototype.hasOwnProperty.call(object, property)) { object = _getPrototypeOf(object); if (object === null) break; } return object; }
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf.bind() : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); Object.defineProperty(subClass, "prototype", { writable: false }); if (superClass) _setPrototypeOf(subClass, superClass); }
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf ? Object.setPrototypeOf.bind() : function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } else if (call !== void 0) { throw new TypeError("Derived constructors may only return object or undefined"); } return _assertThisInitialized(self); }
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf.bind() : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 function _classPrivateMethodInitSpec(obj, privateSet) { _checkPrivateRedeclaration(obj, privateSet); privateSet.add(obj); }
 function _classPrivateFieldInitSpec(obj, privateMap, value) { _checkPrivateRedeclaration(obj, privateMap); privateMap.set(obj, value); }
 function _checkPrivateRedeclaration(obj, privateCollection) { if (privateCollection.has(obj)) { throw new TypeError("Cannot initialize the same private elements twice on an object"); } }
@@ -46657,11 +46664,10 @@ var _clearRef = /*#__PURE__*/new WeakMap();
 var _clear = /*#__PURE__*/new WeakSet();
 var MyListView = /*#__PURE__*/function (_View) {
   _inherits(MyListView, _View);
-  var _super = _createSuper(MyListView);
   function MyListView(props) {
     var _this;
     _classCallCheck(this, MyListView);
-    _this = _super.call(this, props, ['select', 'clear']);
+    _this = _callSuper(this, MyListView, [props, ['select', 'clear']]);
     _classPrivateMethodInitSpec(_assertThisInitialized(_this), _clear);
     _classPrivateFieldInitSpec(_assertThisInitialized(_this), _list, {
       writable: true,
@@ -46673,6 +46679,7 @@ var MyListView = /*#__PURE__*/function (_View) {
     });
     _classPrivateFieldSet(_assertThisInitialized(_this), _clearRef, _react.default.createRef());
     _classPrivateFieldSet(_assertThisInitialized(_this), _list, _this.props.list);
+    console.log('hash', _this.props.list);
     return _this;
   }
   _createClass(MyListView, [{
@@ -47088,6 +47095,16 @@ var ReoApp = /*#__PURE__*/function (_App) {
       _classPrivateFieldSet(_assertThisInitialized(_this), _fontLoaded, true);
       _this.setState({});
     });
+    _Categories.default.forEach(function (cat) {
+      cat.words.forEach(function (word) {
+        if (word.url == '') {
+          // console.log('*', word.maori);
+          word.url = cat.name + '_' + word.maori;
+        }
+      });
+    });
+    console.log('categories');
+    console.log(_Categories.default);
     return _this;
   }
   _createClass(ReoApp, [{
@@ -47220,7 +47237,7 @@ var ReoApp = /*#__PURE__*/function (_App) {
       var label = '';
       switch (view) {
         case 'categories_view':
-          label = 'Select Lists';
+          label = 'Select Practise List(s)';
           break;
         case 'category_view':
           label = 'Add to Custom List';
@@ -47265,6 +47282,7 @@ var ReoApp = /*#__PURE__*/function (_App) {
 function _initCategories2() {
   var _this3 = this;
   var myWords = _Controller.C.getMyDictionary();
+  console.log('my words', myWords);
   var categoryMatch;
   var words = [];
   var match;
@@ -47294,8 +47312,8 @@ function _initCategories2() {
   });
   myWords.categories.forEach(function (category) {
     category.words.forEach(function (word) {
-      // word.selected = true;
-      // this.#myList.words.push(word);
+      word.selected = true;
+      _classPrivateFieldGet(_this3, _myList).words.push(word);
       _classPrivateFieldGet(_this3, _myList).hash[word.url] = category;
     });
   });
