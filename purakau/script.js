d6dbbe0dedf7814c711ec7547aec2201f05ef66d
manac <mcoromandel@hotmail.com>
2025-01-20 13:44:45 +1300

    Add files via upload

--- purakau/script.js
+++ purakau/script.js
@@ -5,6 +5,8 @@ let currentIndex = 0;
 let selectedPhrase = null;
 let detailMode = false;
 let maoriMode = true;
+let categoryVocab;
+let storyVocab;
 
 const ui = {
     buttons: {},
@@ -21,19 +23,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const filePath = "stories/Ngā Haerenga a Tama.json"; // Specify the path to your file
 
     ui.views.categories = getElement('categories-view');
-    ui.displays.categorySelect = getElement('categorySelect');
-    ui.displays.categorySelect.addEventListener('change', (event) => {
-        const selectedIndex = event.target.value;
-        loadCategory(selectedIndex);
-    });
+    // ui.displays.categorySelect = getElement('categorySelect');
+    // ui.displays.categorySelect.addEventListener('change', (event) => {
+    //     const selectedIndex = event.target.value;
+    //     loadCategory(selectedIndex);
+    // });
 
     //load up the views
     ui.views.category = getElement('category-view');
     ui.displays.storySelect = getElement('storySelect');
-    ui.displays.storySelect.addEventListener('change', (event) => {
-        const selectedIndex = event.target.value;
-        loadStory(selectedIndex);
-    });
+    // ui.displays.storySelect.addEventListener('change', (event) => {
+    //     const selectedIndex = event.target.value;
+    //     loadStory(selectedIndex);
+    // });
 
     ui.views.story = getElement('story-view');
 
@@ -49,16 +51,16 @@ document.addEventListener("DOMContentLoaded", function () {
         displaySentence();
     });
 
-    ui.buttons.back = getElement('backButton');
+    ui.buttons.back = getElement('nav-back');
     ui.buttons.back.addEventListener('click', () => {
         if (currentStory.sentences.length > 0 && currentIndex > 0) {
             currentIndex--;
             displaySentence();
             updateButtons();
         }
-    });    
-    
-    ui.buttons.next = getElement('nextButton');
+    });
+
+    ui.buttons.next = getElement('nav-next');
     ui.buttons.next.addEventListener('click', () => {
         if (currentStory.sentences.length > 0 && currentIndex < currentStory.sentences.length - 1) {
             currentIndex++;
@@ -68,60 +70,106 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     ui.displays.sentenceContainer = getElement('sentenceContainer');
-    ui.displays.sentenceContainer.addEventListener('click', () => {
-        if (!detailMode) {
+    ui.displays.sentenceContainer.addEventListener('click', (event) => {
+        if (!event.ctrlKey && !detailMode) {
             maoriMode = !maoriMode;
             displaySentenceSimple();
         }
     });
 
+    ui.views.vocabulary = getElement('vocabulary-view');
+    ui.displays.vocab = getElement('story-vocab');
+    
+    console.log('vocab', ui.views.vocabulary);
+
+    document.addEventListener('selectionchange', () => {
+        if (!detailMode && maoriMode) {
+            const selection = window.getSelection();
+            const selectedText = selection.toString().trim();
+            if (selectedText) {
+                const word = storyVocab[selectedText]
+                console.log('Selected:', word);
+            }
+        }
+    });
+
     ui.displays.detailsContainer = getElement('detailsContainer');
-
-
-    //load category
+    
     fetch('categories.json')
         .then(response => response.json())
         .then(data => {
-            //load up the tuatahi category
-            //`  loadCategory(categories.length-1)            
             categories = data;
-            loadCategories();
+            createSelectionCards(getElement('categories-card-container'), categories, loadCategory);
         })
         .catch(error => {
             console.error("Error loading the file:", error);
         });
-
-
 });
 
-function loadCategories() {
-    //ui.displays.categorySelect
-    // console.log('cat', categories);
-    populateSelect(ui.displays.categorySelect, categories, 'name');
+function createSelectionCards(div, data, handler) {
+    
+    div.innerHTML = '';
+    data.forEach((item, ) => {        
+       const card = document.createElement('div');
+       card.className = 'card';
+       card.innerHTML = '<img src="' + item.image + '" alt="Image"> <div class="title"*>' + item.name + '</div> <div class="description">' + item.description + '</div>';
+       div.appendChild(card);
+
+       card.addEventListener('click', () => {         
+            handler(item);
+       })
+    });    
 }
 
-function loadCategory(index) {
-    if (index != -1) {
-        showView(ui.views.categories, false);
-        showView(ui.views.story, false);
-        showView(ui.views.category, true);
-
-        category = categories[index];
+function loadCategory(category) {
+    
+        showView(ui.views.category);
+    
         document.getElementById('categoryLabel').innerText = category.name;
 
         fetch(category.path)
             .then(response => response.json())
             .then(data => {
-                
-
                 stories = data.stories;
-                populateSelect(ui.displays.storySelect, stories, 'title.maori');
+                //tmp 
+                stories.forEach((story) => {
+                    story.name = story.title.maori;
+                    story.description = '';
+                    story.image = 'images/stock.jpg';
+                });
 
+                categoryVocab = getVocab(data);
+
+                createSelectionCards(getElement('stories-card-container'), stories, loadStory);
             })
             .catch(error => {
                 console.error("Error loading the file:", error);
             });
-    }
+    
+}
+
+function getVocab(category) {
+    const stories = category.stories;
+    ////process the category to add words to the dictionary    
+    let storyVocab;
+    const dict = {};
+
+    stories.forEach((story) => {
+        storyVocab = {};
+        story.vocabulary.forEach((entry) => {
+            if (storyVocab[entry.word] == undefined) {
+                storyVocab[entry.word] = { entries: [] }
+            }
+            storyVocab[entry.word].entries.push({
+                translation: entry.translation,
+                type: entry.explanation
+            });
+
+        })
+        dict[story.title.maori] = storyVocab;
+    })
+
+    return dict;
 }
 
 function populateSelect(select, data, property) {
@@ -135,10 +183,10 @@ function populateSelect(select, data, property) {
     data.forEach((item, index) => {
         const properties = property.split('.')
         let text = item[properties[0]];
-        for(let i = 1; i < properties.length; i++){
+        for (let i = 1; i < properties.length; i++) {
             text = text[properties[i]]
         }
-        
+
         option = document.createElement('option');
         option.value = index;
         option.text = text;
@@ -146,22 +194,21 @@ function populateSelect(select, data, property) {
     });
 }
 
-function loadStory(index) {
-    if (index != -1) {
-        currentStory = stories[index];
+function loadStory(story) {
+    // if (index != -1) {
+        currentStory = story;//stories[index];
         currentIndex = 0;
 
-        showView(ui.views.category, false);
-        showView(ui.views.story, true);
+        storyVocab = categoryVocab[currentStory.title.maori];
+        loadVocab(storyVocab);
+        showView(ui.views.story);
 
-        document.getElementById('storyLabel').innerText = typeof currentStory.title === 'string'? currentStory.title : currentStory.title.maori + ' (' + currentStory.title.english + ')';
-        console.log(currentStory);
+        document.getElementById('storyLabel').innerText = typeof currentStory.title === 'string' ? currentStory.title : currentStory.title.maori + ' (' + currentStory.title.english + ')';
+        console.log('story', currentStory);
 
         displaySentence();
         updateButtons();
-    } else {
-        alert('black');
-    }
+    
 }
 
 function displaySentence() {
@@ -177,7 +224,12 @@ function displaySentence() {
 function displaySentenceSimple() {
 
     const sentence = currentStory.sentences[currentIndex];
-    ui.displays.sentenceContainer.innerHTML = maoriMode ? sentence.maori : sentence.english;
+    ui.displays.sentenceContainer.innerHTML = '';
+    if (maoriMode) {
+        ui.displays.sentenceContainer.innerHTML = sentence.maori;
+    } else {
+        ui.displays.sentenceContainer.innerHTML = sentence.english;
+    }
 }
 
 function displaySentenceDetail() {
@@ -225,10 +277,40 @@ function updateButtons() {
 }
 
 function showIntroView() {
-    showView(ui.views.story, false);
-    showView(ui.views.category, true);
+    showView(ui.views.category);
 }
 
-function showView(view, visible) {
-    view.style.display = visible ? 'flex' : 'none';
+function showVocab() {
+    showView(ui.views.vocabulary);
+}
+
+function closeVocab(){
+    showView(ui.views.story);
+}
+
+function showHome(){
+    showView(ui.views.categories);
+}
+
+function loadVocab(vocab){
+    ui.displays.vocab.innerHTML = '';
+    console.log('load this', vocab);
+    let div;
+    
+    for(let key in vocab){
+        const entry = vocab[key].entries[0];
+        div = document.createElement('div');
+        div.className = 'vocab-word-div';
+        div.innerHTML = key;
+        div.addEventListener('click', () => {
+            alert(entry.type + ': ' + entry.translation);
+        });
+        ui.displays.vocab.appendChild(div);
+    }
+}
+
+function showView(view) {
+    for (let key in ui.views) {
+        ui.views[key].style.display = ui.views[key] == view? 'flex' : 'none';
+    };
 }
